2209f89fdfead82e203737fddd24a04667e489c0
C4n2h4 <51727753+C4n2h4@users.noreply.github.com>
2023-11-13 14:07:41 -0300

    Create desafio02
    Criando calculadora de nível do heroi.

---
+++ desafio02.js
@@ -0,0 +1,28 @@
+function calcularNivel(vitorias, derrotas) {
+    // Calcula o saldo de Rankeadas
+    const saldoVitorias = vitorias - derrotas;
+
+    // Determina o nível com base no saldo de vitórias
+    let nivel;
+    if (vitorias < 10) {
+        nivel = "Ferro";
+    } else if (vitorias <= 20) {
+        nivel = "Bronze";
+    } else if (vitorias <= 50) {
+        nivel = "Prata";
+    } else if (vitorias <= 80) {
+        nivel = "Ouro";
+    } else if (vitorias <= 90) {
+        nivel = "Diamante";
+    } else if (vitorias <= 100) {
+        nivel = "Lendário";
+    } else {
+        nivel = "Imortal";
+    }
+
+    // Exibe a mensagem de saída
+    console.log(`O Herói tem um saldo de ${saldoVitorias} vitórias e está no nível ${nivel}`);
+}
+
+
+calcularNivel(35, 15); 
